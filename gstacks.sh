4193f934b74b85361f6b938e91fd0efd78a1dade
Claire E Schraidt <57269474+ceschraidt@users.noreply.github.com>
2021-03-24 13:33:45 -0400

    Create gstacks.sh

---
+++ gstacks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -A beagle
+#SBATCH --job-name=gstacksv2.5
+#SBATCH -o gstacks.out
+#SBATCH -e gstacks.err
+#SBATCH -N 1
+#SBATCH -n 20
+#SBATCH -t 300:00:00
+#SBATCH -exclusive
+#SBATCH --mail-user= 
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+
+cd $SLURM_SUBMIT_DIR
+
+module load stacks/2.5
+
+gstacks -I ./refalignedbam -M ./popmap.txt -t 1 -O ./gstacks \
+        --rm-unpaired-reads \
+        --details \
